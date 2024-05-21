d078f7c62bf6c09c6c159309b92eec0a3365cde1
MKmatt24 <mkmatt2405@gmail.com>
2024-05-21 23:15:02 +0200

    Manour

--- sansSdl/src/main.c
+++ sansSdl/src/main.c
@@ -9,6 +9,8 @@
 #include <pulse/simple.h>
 #include <pulse/error.h>
 
+
+
 // COMMANDE TERMINAL : gcc -o ProgMain *.c -lncurses -lm -lpthread -lpulse-simple -lpulse
 
 void start_ncurses(bool useRaw, bool useNoecho);
@@ -24,6 +26,19 @@ extern bool stop_music;
 salle room;
 
 int L, C; /*L pour désigner la ligne et C la colonne du click de la souris*/
+
+void print_menu(WINDOW *menu_win, int highlight, int n_choices, char * choices[]);
+
+char *choices[] = { 
+			"Choice 1",
+			"Choice 2",
+			"Choice 3",
+			"Choice 4",
+			"Exit",
+		  };
+    
+
+int n_choices = 5;
 
 void ncurses_initialiser() {
     initscr();          /* Démarre le mode ncurses */
@@ -147,49 +162,53 @@ int main(int argc, char ** argv){
             break;
     }
 
-    // Taille écran
-    int yMax, xMax;
-    getmaxyx(stdscr, yMax, xMax);
-    //création fenêtre//
-    WINDOW = menuwin = newwin(6, xMax-12, yMax-8, 5);
-    box(menuwin, 0, 0);
-    refresh();
-    wresfresh(menuwin);
+    endwin();
+    WINDOW *menu_win;
+    int highlight = 1;
+    int choice = 0;
+    int c = 0;
 
-    keypad(menuwin, true);
-
-    string choices[3] = {"Malk", "Jog", "Run"};
-    int choice;
-    int highlight = 0;
-
-    while(1){
-        for(int i = 0; i < 3; i++){
-           if(i == highlight){
-            wattron(menuwin, A_REVERSE);
-           mvwprintw(menuwin, i+1, 1, choices[i]);
-           } 
-        }    
-        
+	start_x = 10;
+	start_y = 10;
+		
+	menu_win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, start_y, start_x);
+	keypad(menu_win, TRUE);
+	mvprintw(0, 0, "Use arrow keys to go up and down, Press enter to select a choice");
+	refresh();
+	print_menu(menu_win, highlight, n_choices, choices);
+	while(1)
+	{	c = wgetch(menu_win);
+		switch(c)
+		{	case KEY_UP:
+				if(highlight == 1)
+					highlight = n_choices;
+				else
+					highlight--;
+				break;
+			case KEY_DOWN:
+				if(highlight == n_choices)
+					highlight = 1;
+				else 
+					highlight++;
+				break;
+			case 10:
+				choice = highlight;
+				break;
+			default:
+				mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+				refresh();
+				break;
+		}
+		print_menu(menu_win, highlight, n_choices, choices);
+		if(choice != 0){	/* User did a choice come out of the infinite loop */
+			break;
+	}	
+	mvprintw(23, 0, "You chose choice %d with choice string %s\n", choice, choices[choice - 1]);
+	clrtoeol();
+	refresh();
+	endwin();
     }
 
-
-
-
-
-    getch();
-
-
-
-
-
-
-
-
-
-
-
-    
-    
 
     // Boucle pour détecter l'appui sur la touche espace
     ch = 0;
@@ -281,4 +300,24 @@ int main(int argc, char ** argv){
     endwin();
 
     return 0;
+}
+
+void print_menu(WINDOW *menu_win, int highlight, int n_choices, char * choices[])
+{
+	int x, y, i;	
+
+	x = 2;
+	y = 2;
+	box(menu_win, 0, 0);
+	for(i = 0; i < n_choices; i++)
+	{	if(highlight == i + 1) /* High light the present choice */
+		{	wattron(menu_win, A_REVERSE); 
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+			wattroff(menu_win, A_REVERSE);
+		}
+		else
+			mvwprintw(menu_win, y, x, "%s", choices[i]);
+		y++;
+	}
+	wrefresh(menu_win);
 }
--- sansSdl/src/musique.c
+++ sansSdl/src/musique.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <alsa/asoundlib.h>
+
 
 /*
 int musique1(int num_musique) {
